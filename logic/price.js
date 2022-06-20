6713df88d10b01270bd34f3e885db204043dfecc
SMD8 <davidemao86@gmail.com>
2022-06-20 23:26:30 +0200

    Commit module 3

--- logic/price.js
+++ logic/price.js
@@ -1,4 +1,34 @@
 function Prices() {
+  function calculateFinalPrice(basePrice, variationPassengerType, variationFlightType) {
+    let finalPrice = basePrice;
+    finalPrice *= 1 + variationPassengerType/100;
+    finalPrice *= 1 + variationFlightType/100;
+    return finalPrice.toFixed(2);
+  }
 
+  function calculateDefaultFinalPrice(basePrice, passengerType, flightType) {
+    let finalPrice = basePrice;
+    switch(passengerType.toUpperCase()) {
+      case "REGULAR": finalPrice *= 0.95;
+      break;
+      case "VIP": finalPrice *= 1.05;
+      break;
+    }
+    switch(flightType.toUpperCase()) {
+      case "ECONOMY": finalPrice *= 0.97;
+      break;
+      case "BUSINESS": finalPrice *= 1.1;
+      break;
+    }
+    return finalPrice.toFixed(2);
+  }
+
+  function calculateTotalFinalPrice(nrOfSeats, passengerType, flightType, basePrice) {
+    let finalPrice = basePrice;
+    return nrOfSeats * calculateDefaultFinalPrice(basePrice, passengerType, flightType);
+  }
+
+  return {calculateFinalPrice, calculateDefaultFinalPrice, calculateTotalFinalPrice};
 }
 
+module.exports = Prices();
--- logic/util.js
+++ logic/util.js
@@ -16,7 +16,34 @@ return totalDistributedPassengers;
     }
     return totalNumberOfPassengers;
   }
-  return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers};
+
+  function checkInput(input) {
+    if(!input) {
+      throw new Error("There is no input");
+    }
+    if(!Number(input)) {
+      throw new Error("Make sure the input is a number");
+    }
+  }
+
+  function calculateTotalDistance(distancesArray) {
+    let totalDistances = 0;
+    for(distances of distancesArray) {
+      if(distances < 0){
+        continue;
+      }
+      totalDistances += distances;
+    }
+    return totalDistances;
+  }
+
+  function calculateBonusPoints(distanceBusinessSeatsArray, distanceEconomySeatsArray, businessBonusPercentage, economyBonusPercentage) {
+    let businessPoints = calculateTotalDistance(distanceBusinessSeatsArray);
+    let economyPoints = calculateTotalDistance(distanceEconomySeatsArray);
+    let points = (businessBonusPercentage * businessPoints)/100 + (economyPoints * economyBonusPercentage)/100;
+    return points;
+  }
+  return{calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers, checkInput, calculateTotalDistance, calculateBonusPoints};
 }
 
 module.exports = Util();
