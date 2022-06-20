188c222f5bc2903c6c3f3d8b1c3b5420ae2d1127
SMD8 <davidemao86@gmail.com>
2022-06-20 13:12:41 +0200

    first commit

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,12 @@
 function Flights() {
-
+  calculateNumberOfFlights() {
+    if(passengers < 0) {
+      return alert("The number of passengers must be a positive integer value");
+    } else if (capacity < 0) {
+      return alert("The capacity of the flight must be a positive integer number value");
+    }
+  }
+  return {calculateNumberOfFlights}
 }
 
+module.export = Flights();
