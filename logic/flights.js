fb22d190ca459194b0ed810c85b5c2a4ca32d9a9
SMD8 <davidemao86@gmail.com>
2022-06-20 14:09:15 +0200

    Checking work

--- logic/flights.js
+++ logic/flights.js
@@ -6,7 +6,20 @@ function Flights() {
       return alert("The capacity of the flight must be a positive integer number value");
     }
   }
-  return {calculateNumberOfFlights}
+  function checkAircraftRevision(distanceLimit, distanceArray) {
+    if(distance <= distanceLimit/2) {
+      return "The revision needs to be done within the next 3 months";
+    } else if(distance > distanceLimit/2 && distance <= (distanceLimit/4)*3) {
+      return "The revision needs to be done within the next 2 months";
+    } else if(distance > (distanceLimit/4)*3 && distance <= distanceLimit) {
+      return "The revision needs to be done next month";
+    } else if(distance > distanceLimit) {
+      console.error("Too far");
+    }
+  }
+  return {calculateNumberOfFlights, checkAircraftRevision};
 }
+
+
 
 module.export = Flights();
--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,21 @@
 function Passengers() {
-
+  function checkFlightCapacity(flightCapacity, passengersArray) {
+    let passengersNumber = passengersArray.length;
+    if(passengersNumber < flightCapacity) {
+      return passengersNumber;
+    } else {
+      console.error('Too many passengers');
+    }
+  }
+  function distributeAllSeatsToAllPassengers(vipPassengers, regularPassengers, nrOfFlights, businessSeatsPerFlight, economySeatsPerFlight) {
+    return {
+      vipPassengers: businessSeatsPerFlight,
+      vipPassengers: economySeatsPerFlight,
+      regularPassengers: businessSeatsPerFlight,
+      regularPassengers: economySeatsPerFlight
+    }
+  }
+  return {checkFlightCapacity};
 }
 
+module.exports = Passengers();
